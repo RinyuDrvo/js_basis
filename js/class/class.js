0175ab4214a1c437d9206bb3808d27ef96460877
RinyuDrvo <alchemist.drumvocal@gmail.com>
2020-08-30 22:27:34 +0900

    プロトタイプベースのクラス

--- js/class/class.js
+++ js/class/class.js
@@ -4,14 +4,16 @@
  * @param birthday
  */
 function User(name, birthday) {
+  this._name = name;
+  this._birthday = birthday;
+}
 
-  // User内からのみ見える
-  function clacAge() {
-    return new Date().getFullYear() - birthday.getFullYear();
-  }
-  this.sayHi = function () {
-    alert(name + ', age: ' + clacAge());
-  };
+User.prototype._calcAge = function () {
+  return new Date().getFullYear() - this._birthday.getFullYear();
+};
+
+User.prototype.sayHi = function () {
+  alert(this._name + ', age: ' + this._calcAge());
 }
 
 let user = new User("Jhon", new Date(2000, 0, 1));
