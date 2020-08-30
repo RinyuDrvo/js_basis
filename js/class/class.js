7c51335447a4a7d94b6fb4e95e5e324f0f7c9912
RinyuDrvo <alchemist.drumvocal@gmail.com>
2020-08-30 22:21:14 +0900

    プライベートなメソッド追加

--- js/class/class.js
+++ js/class/class.js
@@ -1,12 +1,18 @@
 /**
  * 関数クラスパターン
  * @param name
+ * @param birthday
  */
-function User(name) {
+function User(name, birthday) {
+
+  // User内からのみ見える
+  function clacAge() {
+    return new Date().getFullYear() - birthday.getFullYear();
+  }
   this.sayHi = function () {
-    alert(name);
+    alert(name + ', age: ' + clacAge());
   };
 }
 
-let user = new User("Jhon");
+let user = new User("Jhon", new Date(2000, 0, 1));
 user.sayHi();
