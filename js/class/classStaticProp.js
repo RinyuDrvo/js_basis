560a050a0e917c21038580480c4d9a6388103129
RinyuDrvo <alchemist.drumvocal@gmail.com>
2020-09-02 09:41:25 +0900

    staticメソッドの使用例

--- js/class/classStaticProp.js
+++ js/class/classStaticProp.js
@@ -1,7 +1,21 @@
-class User {
-  static staticMethod() {
-    alert(this == User);
+class Article {
+  constructor(title, date) {
+    this.title = title;
+    this.date = date;
+  }
+
+  static compare(articleA, articleB) {
+    return articleA - articleB;
   }
 }
 
-User.staticMethod();
+// usage
+let articles = [
+  new Article("Mind", new Date(2019, 1, 1)),
+  new Article("Body", new Date(2019, 0, 1)),
+  new Article("JavaScirpt", new Date(2019, 11, 1)),
+];
+
+articles.sort(Article.compare);
+
+alert(articles[0].title);
