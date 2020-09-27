04393a0bd36cb19746fc26c3d6b0e3d5a2637afe
RinyuDrvo <alchemist.drumvocal@gmail.com>
2020-09-27 17:57:07 +0900

    make showCircle func

--- js/animation_circle/script.js
+++ js/animation_circle/script.js
@@ -1,3 +1,12 @@
 function showCircle(cx, cy, radius) {
+  const circle = document.getElementById('circle');
+  circle.style.width = 0;
+  circle.style.height = 0;
+  circle.style.left = cx + 'px';
+  circle.style.top = cy + 'px';
 
+  setTimeout(() => {
+    circle.style.width = radius * 2 + 'px';
+    circle.style.height = radius * 2 + 'px';
+  }, 0)
 }
