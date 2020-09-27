f81eb7b82d789a0cba9dbde77f9203a9347a89a9
RinyuDrvo <alchemist.drumvocal@gmail.com>
2020-09-27 22:58:13 +0900

    use promise

--- js/animation_circle/script.js
+++ js/animation_circle/script.js
@@ -1,24 +1,26 @@
-function showCircle(cx, cy, radius, callback) {
+function showCircle(cx, cy, radius) {
   const circle = document.getElementById('circle');
   circle.style.width = 0;
   circle.style.height = 0;
   circle.style.left = cx + 'px';
   circle.style.top = cy + 'px';
 
-  setTimeout(() => {
-    circle.style.width = radius * 2 + 'px';
-    circle.style.height = radius * 2 + 'px';
+  return new Promise(resolve => {
+    setTimeout(() => {
+      circle.style.width = radius * 2 + 'px';
+      circle.style.height = radius * 2 + 'px';
 
-    circle.addEventListener('transitionend', function handler() {
-      circle.removeEventListener('transitionend', handler);
-      callback(circle);
-    });
-  }, 0)
+      circle.addEventListener('transitionend', function handler() {
+        circle.removeEventListener('transitionend', handler);
+        resolve(circle);
+      });
+    }, 0)
+  })
 }
 
 const button = document.getElementById('button');
 button.onclick = function () {
-  showCircle(250, 250, 100, div => {
+  showCircle(250, 250, 100).then(div => {
     div.classList.add('message-ball');
     div.append("Hello World");
   });
